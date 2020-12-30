b6b5e6bdcd4cf1dd30da58e16103441bcbbf749e
Jirayut <jirayut.patt@gmail.com>
2020-12-30 13:07:06 +0700

    fig cors error

--- src/main/java/com/alli/backend/BackendApplication.java
+++ src/main/java/com/alli/backend/BackendApplication.java
@@ -1,7 +1,5 @@
 package com.alli.backend;
 
-import com.alli.backend.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
--- src/main/java/com/alli/backend/controllers/ProductController.java
+++ src/main/java/com/alli/backend/controllers/ProductController.java
@@ -3,10 +3,7 @@ package com.alli.backend.controllers;
 import com.alli.backend.models.Product;
 import com.alli.backend.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
